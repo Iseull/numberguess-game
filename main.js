941b95e139374d546d6aa6c8507bbf8c6907d963
Iseul Kim <mary061800@naver.com>
2024-04-08 03:11:42 +0900

    add new sentence

--- main.js
+++ main.js
@@ -13,8 +13,18 @@ let playButton=document.getElementById("play-button");
 //document 이 웹사이트 자체이고, 플레이버튼이란 아이디(클래스같은것도 o)를 선택한다는 의미
 let userInput=document.getElementById("user-input");
 let resultArea=document.getElementById("result-area");
+let resetButton=document.getElementById("reset-button");
+let chances=5;
+let gameOver=false;
+let chanceArea=document.getElementById("chance-area");
+let history=[];
+
 
 playButton.addEventListener("click",play);
+resetButton.addEventListener("click",reset);
+userInput.addEventListener("focus",function(){
+    userInput.value="";
+}); //잠깐 쓰고 버릴꺼라 함수이름 x
 
 //addEventListener(이벤트이름, 이벤트 발생시 실행함수) 이 버튼에 이벤트 더해주는
 //함수도 매개변수처럼 넘길 수 있음 그래서 play()하면 안됨
@@ -27,6 +37,21 @@ function pickRandomNum(){
 function play(){
     let userValue=userInput.value;
 
+    if(userValue<1||userValue>100)
+    {
+        resultArea.textContent="1과 100 사이 숫자를 입력해주세요";
+        return;
+    }
+
+if(history.includes(userValue)){
+    resultArea.textContent="이미 입력한 숫자입니다 다른 숫자를 입력해 주세요"
+    return;
+}
+
+    chances --;
+
+    chanceArea.textContent=`남은 기회 : ${chances}번`;
+
     if(userValue<computerNum){
         resultArea.textContent = "UP!!!";
     }else if(userValue>computerNum){
@@ -34,6 +59,27 @@ function play(){
     }
     else{
         resultArea.textContent = "맞추셨습니다!!!";
+        gameOver=true;
+    }
+
+    history.push(userValue);
+    console.log(history);
+    
+    if(chances<1){
+        gameOver=true;
+    }
+    if(gameOver==true){
+        playButton.disabled=true;
     }
 }
+
+function reset(){
+    //user input창이 깨끗하고 정리되고
+    userInput.value="";
+    //새로운 번호가 생성되고
+    pickRandomNum();
+
+    resultArea.textContent="결과값이 여기 나옵니다!!!";
+}
+
 pickRandomNum();
